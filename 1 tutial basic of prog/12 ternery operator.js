f5ea3117b3d4668df0e96f4f07eb011b32af24d9
Umair_Jami <lalaumair92@gmail.com>
2024-04-11 21:56:47 +0500

    Updated README.md

--- 1 tutial basic of prog/12 ternery operator.js
+++ 1 tutial basic of prog/12 ternery operator.js
@@ -11,8 +11,8 @@
 // console.log(drink);
 
 // this is ternery operator
-let age=2;
-let drink= age>=5? 'coffee' : 'milk';
+let age = 2;
+let drink = age >= 5 ? "coffee" : "milk";
 
 console.log(drink);
 // there if condition is true then value after ? will print or if the condition is false then value after : will print
