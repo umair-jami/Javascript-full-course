06d327813ba7982caf55ec998a4c8fc174f201d2
Umair_Jami <lalaumair92@gmail.com>
2024-04-13 19:56:31 +0500

    Updated Code

--- 2 tutorial 3Arrays and object/30 array destructuring.js
+++ 2 tutorial 3Arrays and object/30 array destructuring.js
@@ -1,12 +1,12 @@
 // array destructuring
-const myarray =['value1','value2','value3','value4']
+const myarray = ["value1", "value2", "value3", "value4"];
 // let myvar1 = myarray[0]
 // let myvar2 = myarray[1]
 
-//for inserting item in new array we can use slice or spread method 
-let [myvar1,myvar2,...mynewarray] = myarray;
-console.log('value of myvar1', myvar1)
-console.log('value of myvar2', myvar2)
-let mynewarray2 = myarray.slice(2)
-console.log(mynewarray)
-console.log(mynewarray2)
+//for inserting item in new array we can use slice or spread method
+let [myvar1, myvar2, ...mynewarray] = myarray;
+console.log("value of myvar1", myvar1);
+console.log("value of myvar2", myvar2);
+let mynewarray2 = myarray.slice(2);
+console.log(mynewarray);
+console.log(mynewarray2);
