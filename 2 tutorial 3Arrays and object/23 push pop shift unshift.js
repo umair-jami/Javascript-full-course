cd3a14f1f5e68721790ca024ccfde3ff7bf108f5
Umair_Jami <lalaumair92@gmail.com>
2024-04-13 12:06:41 +0500

    Updated Code

--- 2 tutorial 3Arrays and object/23 push pop shift unshift.js
+++ 2 tutorial 3Arrays and object/23 push pop shift unshift.js
@@ -1,9 +1,6 @@
 //array push pop
 
-
-
 //push---->for adding element in last
-
 
 // let fruits=['apple','mango','grapes']
 // console.log(fruits)
@@ -11,29 +8,27 @@
 // console.log(fruits)
 
 //pop----->for removing element from last one by one and it also return elements
-let fruits=['apple','mango','grapes']
+let fruits = ["apple", "mango", "grapes"];
 // console.log(fruits)
 // fruits.pop();
 // console.log(fruits)
 
 //pooped items
 
-
 // let poppedfruit = fruits.pop()
 // console.log(fruits)
 // console.log('popped fruit is',poppedfruit)
 
-
 //Array shift unshift
 
 //unshift ----->for adding element in the start
-fruits.unshift('ananas')
-console.log(fruits)
+fruits.unshift("ananas");
+console.log(fruits);
 
 //shift--->for removing elements from the start
 
-let shiftfruit= fruits.shift()
-console.log(fruits)
-console.log(shiftfruit)
+let shiftfruit = fruits.shift();
+console.log(fruits);
+console.log(shiftfruit);
 
-//push and pops are fast as compared to shift and unshift because sift and unshift just add and remove element in the start and push and pop add and remove element from the last for the they change postion of first item and then add second item
+//push and pops are fast as compared to shift and unshift because shift and unshift just add and remove element in the start and push and pop add and remove element from the last for the they change postion of first item and then add second item
