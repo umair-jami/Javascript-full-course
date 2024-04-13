6c6aa0e1a02efcfe85c5bcfc077c363205e92745
Umair_Jami <lalaumair92@gmail.com>
2024-04-13 08:18:53 +0500

    Updated Code

--- 1 tutial basic of prog/19 for loop.js
+++ 1 tutial basic of prog/19 for loop.js
@@ -5,23 +5,18 @@
 // }
 // console.log(i)
 
-
 // if we use 'var' instead of "let" then it exicute outside
-
-
 
 // for(var i=0; i<=9;i++){
 //     console.log(i)
 // }
 // console.log("value of i is ", i)
 
-
-
 // exp
 let total = 0;
 let num = 10;
-for(let i = 1; i<=num; i++){
-    total=total + i;
+for (let i = 1; i <= num; i++) {
+  total = total + i;
 }
 console.log(total);
 
