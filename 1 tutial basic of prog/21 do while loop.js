7eb51ec6e72bbafa63960e5cf8962b9d1970d818
Umair_Jami <lalaumair92@gmail.com>
2024-04-13 08:24:44 +0500

    Updated Code

--- 1 tutial basic of prog/21 do while loop.js
+++ 1 tutial basic of prog/21 do while loop.js
@@ -6,9 +6,9 @@
 // }
 
 // do mean mujhy tab tak ye kam kerna hai jab tak while ky ander value condition false nni hojati
-let i=10;
-do{
-    console.log(i);
-    i++;
-}while(i<=9);
-console.log("value of i is",i)
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i <= 9);
+console.log("value of i is", i);
