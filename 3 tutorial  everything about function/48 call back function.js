9ebace21ad64b660d58dbfc0132137287bac1f27
Umair_Jami <lalaumair92@gmail.com>
2024-04-19 10:10:18 +0500

    Updated README.md

--- 3 tutorial  everything about function/48 call back function.js
+++ 3 tutorial  everything about function/48 call back function.js
@@ -1,20 +1,17 @@
 //call back function
 
-function myfunc2(name){
-    console.log('inside myfun2')
-    console.log(`your name is ${name}`)
+function myfunc2(name) {
+  console.log("inside myfun2");
+  console.log(`your name is ${name}`);
 }
-
 
 //there we can set any value of 'a' like string, array, number
 // function myfunc(a){
 //     console.log(a)
 // }
 
-
-
-function myfunc(callback){
-    console.log('hlo i am a function and i  can....')
-    callback('umair')
+function myfunc(callback) {
+  console.log("hlo i am a function and i  can....");
+  callback("umair");
 }
-myfunc(myfunc2) 
+myfunc(myfunc2);
