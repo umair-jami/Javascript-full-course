592e8ae0de94862d6a069d3a2234ffe2abd6214c
Umair_Jami <lalaumair92@gmail.com>
2024-04-20 18:28:48 +0500

    Updated Code

--- 5 set and map in javascript/59 optional chaining.js
+++ 5 set and map in javascript/59 optional chaining.js
@@ -1,8 +1,8 @@
 //optional chaining
 
 const user = {
-    firstname:'umair',
-    // address: {housenumber: '1234'}
-}
-console.log(user?.firstname)
-console.log(user?.address?.housenumber)
+  firstname: "umair",
+  // address: {housenumber: '1234'}
+};
+console.log(user?.firstname);
+console.log(user?.address?.housenumber);
