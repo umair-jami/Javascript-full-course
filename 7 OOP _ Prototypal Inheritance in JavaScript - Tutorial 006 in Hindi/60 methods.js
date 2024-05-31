6d7b037adb14ca7b6a2c9b95aaa9c3763fb2b176
Umair_Jami <lalaumair92@gmail.com>
2024-05-31 19:58:15 +0500

    Updated README.md

--- 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/60 methods.js
+++ 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/60 methods.js
@@ -1,34 +1,36 @@
 // methods
 //function inside object are calles method
 //iss function mai this wo object hai jo is function ko call ker raha hai
-const person ={
-    firstname:'umair',
-    age:8,
-    about:function(){
-        console.log(`person name is ${this.firstname} and person age is ${this.age}`)
-    }
-}
+const person = {
+  firstname: "umair",
+  age: 8,
+  about: function () {
+    console.log(
+      `person name is ${this.firstname} and person age is ${this.age}`
+    );
+  },
+};
 person.about();
 
 //another example
-function personinfo(){
-    console.log(`person name is ${this.firstname} and person age is ${this.age}`)
+function personinfo() {
+  console.log(`person name is ${this.firstname} and person age is ${this.age}`);
 }
-const person1={
-    firstname:'umar',
-    age:18,
-    about:personinfo
-}
-const person2={
-    firstname:'Raza',
-    age:10,
-    about:personinfo
-}
-const person3={
-    firstname:'ubaid',
-    age:9,
-    about:personinfo
-}
-person1.about()
-person2.about()
-person3.about()
+const person1 = {
+  firstname: "umar",
+  age: 18,
+  about: personinfo,
+};
+const person2 = {
+  firstname: "Raza",
+  age: 10,
+  about: personinfo,
+};
+const person3 = {
+  firstname: "ubaid",
+  age: 9,
+  about: personinfo,
+};
+person1.about();
+person2.about();
+person3.about();
--- 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/67 __proto__.js
+++ 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/67 __proto__.js
@@ -8,19 +8,20 @@
 //prototype is  different
 
 //there in anothe way to creat object
-const obj1={
-    key1:'value1',
-    key2:'value2'
-}
+const obj1 = {
+  key1: "value1",
+  key2: "value2",
+};
 // const obj2={
 //     key3:'value3'
 // }
 // mai chahta hon ky ky agar obj2 ky pas key1 nahi hai to wo khud obj1 ky pas jai or key1 print kerwai
 // console.log(obj2.key1)
+//.__proto__ is also known as dunder
+// dunder proto === __proto__ Every object in JS has this property
 
-
-const obj2=Object.create(obj1);
+const obj2 = Object.create(obj1);
 //there object.creat ny obj2 ki __proto__ obj1 set kerdi
-obj2.key3='value3'
+obj2.key3 = "value3";
 //there is not present key2 in obj2 but we creat obj2 in this way that if obj2 not have key2 than system go to obj1 and print value of key2
-console.log(obj2.key2)
+console.log(obj2.key2);
