5e85d12668f4e298b4a8d3e042f42f3e8a7d8b78
Umair_Jami <lalaumair92@gmail.com>
2024-06-03 12:57:57 +0500

    Updated code

--- 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/77 getters and setters.js
+++ 7 OOP _ Prototypal Inheritance in JavaScript - Tutorial 006 in Hindi/77 getters and setters.js
@@ -1,31 +1,23 @@
 //getters and setters
-class person{
-    constructor(firstname,lastname,age){
-        this.firstname=firstname
-        this.lastname=lastname
-        this.age=age
-    }
-    get fullname(){
-        return `${this.firstname} ${this.lastname}`
-    }
-    SetName(firstname,lastname){
-        this.firstname=firstname
-        this.lastname=lastname
-    }
+class person {
+  constructor(firstname, lastname, age) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+  }
+  get fullname() {
+    return `${this.firstname} ${this.lastname}`;
+  }
+  //agar hum chahty hai ky simple person1.fullname='umair raza' likhy or name change ho jai to hum ko set method use kerna chahiye
 }
 
-//if we want to change first and last name of object after creation then we use 'SetName' 
-const person1=new person('umair','ramzan',18)
-console.log(person1)
+//if we want to change first and last name of object after creation then we use 'SetName'
+const person1 = new person("umair", "ramzan", 18);
+console.log(person1);
 //if we want to call method function without parenthesis mean fullname without () then we use get in the start of method
-console.log(person1.fullname)
+console.log(person1.fullname);
 // person1.SetName('raza','somroo')
-//we also use this 
-person1.firstname='raza'
-person1.lastname='somroo'
-console.log(person1.fullname)
-
-
-
-
-
+//we also use this
+person1.firstname = "raza";
+person1.lastname = "somroo";
+console.log(person1.fullname);
