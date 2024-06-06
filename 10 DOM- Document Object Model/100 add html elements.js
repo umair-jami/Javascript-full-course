34b095374e3781adcb7faca707c9c0a6d142c2e1
Umair_Jami <lalaumair92@gmail.com>
2024-06-06 15:09:26 +0500

    Updated Code

--- 10 DOM- Document Object Model/100 add html elements.js
+++ 10 DOM- Document Object Model/100 add html elements.js
@@ -1,19 +1,18 @@
 //add new HTML elements to page
 
 //innerHTML to add html element
-const todolist=document.querySelector('.todo-list')
+const todolist = document.querySelector(".todo-list");
 // console.log(todolist.innerHTML)
 
 //agar hum inner html add kerain gy to wo pehly wali html ko remove kery ga or phir new html add hogi
-//agar hum chahaty hai ky purani html bhi rahy and new html bhi add ho gai 
+//agar hum chahaty hai ky purani html bhi rahy and new html bhi add ho gai
 //lakin ye tariqa sahi nahi hai ye masla kery ga
 
 // todolist.innerHTML += <li>New todo</li>
 // console.log(todolist.innerHTML)
 
-todolist.innerHTML='<li> new todo</li>'
-
+todolist.innerHTML = "<li> new todo</li>";
 
 //when you should use it ,when you should not
 
-//agar hum koi innerhtml change kerna chahty hai to hum jer sakty hia or agar new inner html add kerna chayte kery gy to system mai masla kery ga
+//agar hum koi innerhtml change kerna chahty hai to hum use ker sakty hai or agar new inner html add kery gy to system mai masla kery ga
--- 4 Most important arrays mehtod/48 reduce method.js
+++ 4 Most important arrays mehtod/48 reduce method.js
@@ -1,16 +1,15 @@
 //reduce method
-const numbers=[1,2,3,4,5]
+const numbers = [1, 2, 3, 4, 5];
 
 //aim: sum of all the numbers in array
 
 // this is mentiones in documentation tha t the first parameter name will be accumulator and the second parameter called current value
 
 //we can set initial value of accumulator at the end of function as mentioned 100
-const sum=numbers.reduce((accumulator,currentvalue)=>{
-    return accumulator+currentvalue
-},100);
-console.log(sum)
-
+const sum = numbers.reduce((accumulator, currentvalue) => {
+  return accumulator + currentvalue;
+}, 100);
+console.log(sum);
 
 //method
 // accumulator,     currentvalue,       return
@@ -21,15 +20,13 @@ console.log(sum)
 
 //real example
 
-const usercart =[
-    {productid:1,productname:'mobile',price:12000},
-    {productid:1,productname:'laptope',price:22000},
-    {productid:1,productname:'Tv',price:15000},
-]
+const usercart = [
+  { productid: 1, productname: "mobile", price: 12000 },
+  { productid: 1, productname: "laptope", price: 22000 },
+  { productid: 1, productname: "Tv", price: 15000 },
+];
 
-const price=usercart.reduce((totalvalue, currentproduct)=>{
-    return totalvalue  + currentproduct.price
-},0)
-console.log(price)
-
-
+const price = usercart.reduce((totalvalue, currentproduct) => {
+  return totalvalue + currentproduct.price;
+}, 0);
+console.log(price);
