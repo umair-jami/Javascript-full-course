21693e8ca9992cc1743ca659ccde10a22040fc83
Umair_Jami <lalaumair92@gmail.com>
2024-07-12 07:07:00 +0500

    Updated code

--- 13 JavaScript Promise , Async Await, Fetch , XHR - JavaScript tutorial 012 in Hindi/7 jsonPractise.js
+++ 13 JavaScript Promise , Async Await, Fetch , XHR - JavaScript tutorial 012 in Hindi/7 jsonPractise.js
@@ -12,6 +12,8 @@ xhr.open("GET", URL);
 //     console.log(typeof data);
 //   }
 // };
+
+//ye onload function tabhi chaly ga jab readyState 4 ho jai gi
 xhr.onload = function () {
   const response = xhr.response;
   const data = JSON.parse(response);
@@ -30,3 +32,21 @@ xhr.send();
 //     console.log(typeof data);
 //   }
 // };
+
+
+// staus code list
+// 1xx: informational response – the request was received, continuing process
+// 2xx: successful – the request was successfully received, understood, and accepted
+// 3xx: redirection – further action needs to be taken in order to complete the request
+// 4xx: client error – the request contains bad syntax or cannot be fulfilled
+// 5xx: server error – the server failed to fulfil an apparently valid request
+
+
+// readyStates
+
+// Value	State	    Description
+// 0	   (UNSENT)	Client has been created. open() not called  yet.
+// 1	   (OPENED)	open() has been called.
+// 2	  (HEADERS_RECEIVED)	send() has been called,     and  headers and status are available.
+// 3	  (LOADING)	Downloading; responseText holds partial data.
+// 4	  (DONE)	The operation is complete. 
