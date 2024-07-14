d2b5bb268bf070c993c8f6dceeffc97702aa7c2e
Umair_Jami <lalaumair92@gmail.com>
2024-07-14 12:58:20 +0500

    Updated Code

---
+++ 4 Most important arrays mehtod/55 Summary.js
@@ -0,0 +1,18 @@
+//forEach method works as a for loop
+//map method works same but it return vlaue and we can store this value in variable and then console it
+
+//filter method reutrn All True values
+
+//reduce method use for adding all number in the array it takes two parameters first accumulator and second current value and we can set value of accumulator zero(0)
+
+//sort method sort item of array it also use two parameters a and b and subtract them if ans is positive value then it set b value first and a value second and vice versa
+
+//find method return first true value
+
+//every method return in boolean value. if condition true over all then return true otherwise false 
+
+//some method also return in boolean value.if conditon is true for one value then it return true if condition is not true for any of them then it return false
+
+//fill method use to fill array with specific elements and it also replace element of array it takes 3 values first for replacing elemetn ,second for Starting index and third for last index
+
+//splice method use for removing and adding element in the array it takes 3 values first starting indes ,second for number of deleting element and third for adding elements
